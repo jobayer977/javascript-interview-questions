10c61adc9bbdf3ac82d258cef38f84324844357a
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-04 23:41:54 +0600

    docs(Topic): :fire: What is use strict mode?

--- exp/index.js
+++ exp/index.js
@@ -1,22 +1,4 @@
-const getNumber = new Promise((resolve, reject) => {
-	setInterval(() => {
-		resolve(10);
-	}, 100);
-});
-getNumber
-	.then((number) => {
-		console.log(number); // 11
-		return number + 1;
-	})
-	.then((number) => {
-		console.log(number); // 12
-		return number + 1;
-	})
-	.then((number) => {
-		console.log(number);
-		return number + 1; // 13
-	});
-
-// 11
-// 12
-// 13
+'use strict';
+username = 'Jon';
+console.log(username);
+// Uncaught ReferenceError: username is not defined
