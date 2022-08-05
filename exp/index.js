98b0ec6ca0e9a85f51ed0cb82785ebc0d9b44084
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-05 19:35:41 +0600

    docs(Topic): :fire: What does "!--" do in JavaScript?

--- exp/index.js
+++ exp/index.js
@@ -1,9 +1,14 @@
 const obj = {
 	a: 1,
 	b: 2,
-	c: 3
+	c: 3,
+	d: 4,
+	e: 5,
+	f: 6
 };
-
+let n = 1;
 for (let key of Object.entries(obj)) {
-	console.log(key);
+	if (!--n) {
+		console.log(n);
+	}
 }
