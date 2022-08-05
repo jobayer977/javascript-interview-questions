1f6891e43d446e4aae077a04b97792d174ff609f
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-05 19:49:20 +0600

    docs(Topic): :fire: What is polyfill used for?

--- exp/index.js
+++ exp/index.js
@@ -1,14 +1,6 @@
-const obj = {
-	a: 1,
-	b: 2,
-	c: 3,
-	d: 4,
-	e: 5,
-	f: 6
-};
-let n = 1;
-for (let key of Object.entries(obj)) {
-	if (!--n) {
-		console.log(n);
+for (let i = 0; i < 10; i++) {
+	if (i !== 5) {
+		continue;
 	}
+	console.log(i);
 }
