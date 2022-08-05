e9714ce26e05075607cb7644be9324921c811177
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-05 18:22:45 +0600

    docs(Topic): :fire: Why do we use setInterval in JavaScript?

--- exp/index.js
+++ exp/index.js
@@ -1,6 +1,1 @@
-function greeting(name, role) {
-	console.log(`Hello, my name is ${name}`);
-	console.log(`I'm a ${role}`);
-}
-
-setTimeout(greeting, 3000, 'Nathan', 'Software developer');
+// setInterval  example
