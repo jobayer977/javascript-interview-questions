900f395a5c43d7940059761b71f7cd087ce6da1e
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-05 18:17:15 +0600

    docs(Topic): :fire: What is the purpose of setTimeout() function in JavaScript ?

--- exp/index.js
+++ exp/index.js
@@ -1,5 +1,6 @@
-function getUserInfo() {
-	debugger;
+function greeting(name, role) {
+	console.log(`Hello, my name is ${name}`);
+	console.log(`I'm a ${role}`);
 }
 
-getUserInfo();
+setTimeout(greeting, 3000, 'Nathan', 'Software developer');
