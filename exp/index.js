f77e2830155e105ff04c86054ed2c4dd0c59f737
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-13 10:52:08 +0600

    main: Added

---
+++ exp/index.js
@@ -0,0 +1,4 @@
+// What is Map object?
+// Ans: Map is an object that holds key-value pairs. Each key is unique in the map. The value can be any type, even another map. Map is an iterable object. It has a forEach() method, which can be used to iterate over all key-value pairs in the map. Map is a collection of key-value pairs.
+
+// Example
