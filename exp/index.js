36b92ea6fd661a5405a962049d9d758846522482
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-14 21:10:17 +0600

    main: Added

--- exp/index.js
+++ exp/index.js
@@ -1,11 +1,35 @@
-function dragstart_handler(ev) {
-	// Add the target element's id to the data transfer object
-	ev.dataTransfer.setData('text/plain', ev.target.id);
-}
+window.addEventListener('load', function () {
+	// Function de display info about the storage space
+	function dislayStorageSpace() {
+		navigator.storage.estimate().then(function (estimate) {
+			console.info('Estimated quota: ', estimate.quota);
+			console.info('Estimated usage: ', estimate.usage);
+			const percent = ((estimate.usage / estimate.quota) * 100).toFixed(2);
+			console.info('Your currently used storage: ', persent, '%');
+		});
+	}
 
-window.addEventListener('DOMContentLoaded', () => {
-	// Get the element by id
-	const element = document.getElementById('p1');
-	// Add the ondragstart event listener
-	element.addEventListener('dragstart', dragstart_handler);
+	// Check if a storage is persistent
+	function isStoragePersistent() {
+		if (navigator.storage && navigator.storage.persisted) {
+			navigator.storage.persisted().then(function (persisted) {
+				const isPersistent = persisted ? true : false;
+				console.info('Is my storage persistent? ', isPersistent);
+			});
+		}
+	}
+
+	dislayStorageSpace();
+	isStoragePersistent();
+
+	// Request storage persistence from the browser
+	document.getElementById('btnReqPers').addEventListener('click', function () {
+		navigator.storage.persist().then(function (result) {
+			if (result) {
+				console.log("This site's storage has been made persistent");
+			} else {
+				console.log("Unable to make this site's storage persistent");
+			}
+		});
+	});
 });
