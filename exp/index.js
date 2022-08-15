cd96dc4450731f644c8bd72dea0a365dff74743b
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-15 21:32:27 +0600

    refactor(staucture): :zap: Split into modules guide

--- exp/index.js
+++ exp/index.js
@@ -1,18 +1,2 @@
-/*
- * Complete the isPositive function.
- * If 'a' is positive, return "YES".
- * If 'a' is 0, throw an Error with the message "Zero Error"
- * If 'a' is negative, throw an Error with the message "Negative Error"
- */
-function isPositive(a) {
-	if (a > 0) {
-		return 'YES';
-	} else if (a === 0) {
-		throw new Error('Zero Error');
-	} else {
-		throw new Error('Negative Error');
-	}
-}
-
-// Explanation
-// The if statement checks if 'a' is greater than 0. If it is, the function returns 'YES'. If 'a' is 0, the function throws an error with the message 'Zero Error'. If 'a' is less than 0, the function throws an error with the message 'Negative Error'. The else statement is not needed because the function will never reach the else statement if 'a' is 0 or greater than 0.
+// Can I redeclare let and const variables?
+// ANSWER : Yes, you can. You can redeclare a variable if you want to change its value. You can also redeclare a variable if you want to change its type. You can redeclare a variable if you want to change its scope.
