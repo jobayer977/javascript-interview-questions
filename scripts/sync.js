c1a4396852ecce6b0c6f7f2d795d6c91adca9581
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-19 11:01:01 +0600

    main: Added

--- scripts/sync.js
+++ scripts/sync.js
@@ -47,7 +47,7 @@ import path from 'path';
 		.join('');
 	const topicsStringForMarkdown = Object.entries(topics)
 		.map((x, i) => {
-			return `# ${x[0]}\n ${x[1]
+			return `## ${x[0]}\n ${x[1]
 				.map((y, yIndex) => `\n ## ${i + 1}.${yIndex + 1} ${y?.title} \n ${y?.content} \n`)
 				.join('\n   ')}\n`;
 		})
